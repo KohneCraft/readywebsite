c28b5ba4f81b81507e6d2b00e66988a01cb5ed58
Yusuf Kandemir <109743732+KohneCraft@users.noreply.github.com>
2026-01-01 01:01:39 +0300

    Delete src/components directory

--- src/components/pageBuilder/renderers/index.ts
+++
@@ -1,5 +0,0 @@
-export { PageRenderer } from './PageRenderer';
-export { SectionRenderer } from './SectionRenderer';
-export { ColumnRenderer } from './ColumnRenderer';
-export { BlockRenderer } from './BlockRenderer';
-
