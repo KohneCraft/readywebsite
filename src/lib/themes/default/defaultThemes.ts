17bcf0cd801f4d7d8ded7b01efa3697d79713c45
Yusuf Kandemir <109743732+KohneCraft@users.noreply.github.com>
2026-01-20 14:35:50 +0300

    Add files via upload

--- src/lib/themes/default/defaultThemes.ts
+++ src/lib/themes/default/defaultThemes.ts
@@ -8,12 +8,25 @@ import { modernBusinessTheme } from '../ModernBusiness/modernBusinessTheme';
 import { minimalTheme } from '../Minimal/minimalTheme';
 import { corporateTheme } from '../Corporate/corporateTheme';
 import { constructionTheme } from '../Construction/constructionTheme';
+import { restoranTheme } from '../Restoran/restoranTheme';
+import { saglikTheme } from '../Saglik/saglikTheme';
+import { egitimTheme } from '../Egitim/egitimTheme';
+import { eticaretTheme } from '../Eticaret/eticaretTheme';
 
 /**
  * Tüm varsayılan temaları getir
  */
 export function getDefaultThemes(): ThemeData[] {
-  return [modernBusinessTheme, minimalTheme, corporateTheme, constructionTheme];
+  return [
+    modernBusinessTheme,
+    minimalTheme,
+    corporateTheme,
+    constructionTheme,
+    restoranTheme,
+    saglikTheme,
+    egitimTheme,
+    eticaretTheme,
+  ];
 }
 
 /**
@@ -31,4 +44,7 @@ export { modernBusinessTheme } from '../ModernBusiness/modernBusinessTheme';
 export { minimalTheme } from '../Minimal/minimalTheme';
 export { corporateTheme } from '../Corporate/corporateTheme';
 export { constructionTheme } from '../Construction/constructionTheme';
-
+export { restoranTheme } from '../Restoran/restoranTheme';
+export { saglikTheme } from '../Saglik/saglikTheme';
+export { egitimTheme } from '../Egitim/egitimTheme';
+export { eticaretTheme } from '../Eticaret/eticaretTheme';
